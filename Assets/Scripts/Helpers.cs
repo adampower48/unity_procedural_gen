0504478a8fbc2ebeac5ef1fecd89e19183ab70be
adampower48 <adam.power48@mail.dcu.ie>
2019-10-19 17:28:05 +0100

    Added function to separate faces sharing vertices in meshes. This fixes normals so that edges on the mesh are visible.

--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -21,4 +21,40 @@ public static class Helpers
         // min inclusive, max exclusive
         return (val - min) % (max - min) + min;
     }
+
+    public struct MeshInfo
+    {
+        public Vector3[] vertices;
+        public int[] triangles;
+    }
+
+    public static MeshInfo FixMesh(Vector3[] verts, int[] tris)
+    {
+        // Slit faces using the same vertices so they are independent.
+
+        var triSet = new HashSet<int>();
+        var vertices = new List<Vector3>(verts);
+
+        for (var i = 0; i < tris.Length; i++)
+        {
+            var tri = tris[i];
+            if (triSet.Contains(tri))
+            {
+                // Create new vert at same location
+                vertices.Add(verts[tri]);
+                // Change triangle to use new vert
+                tris[i] = vertices.Count - 1;
+            }
+            else
+            {
+                triSet.Add(tri);
+            }
+        }
+
+        return new MeshInfo
+        {
+            vertices = vertices.ToArray(),
+            triangles = tris
+        };
+    }
 }
